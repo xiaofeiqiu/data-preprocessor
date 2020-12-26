a7a8cd6f0c892fee7dbc6a0cf5b6873fbebae634
Nathan <xqiu35@gmail.com>
2020-12-26 11:59:21 -0500

    add cols

--- README.md
+++ README.md
@@ -6,13 +6,23 @@ Todo
 3. Aroon 50 up, and Aroon 50 down
 4. macd 20,200,100
 
-ema20_N ema50_N ema100_N ema200_N CCI_N aroon-up_N aroon-down_N macd_N
+
+Raw data
+ema20
+ema50
+ema100
+ema200
+
+
+
+
+ema20_N ema50_N ema100_N ema200_N CCI_N aroon-up_N aroon-down_N macd_N macd_sig
 
 
 emaX_N = (emaX - emaMin)/(emaMax-emaMin)  
 
 CCI_N= cci/100
 
-Aroon_N = Aroon / 50
+Aroon_N = Aroon / 100
 
 MAC= hist/sig
--- services/dbservice/types.go
+++ services/dbservice/types.go
@@ -1,28 +1,27 @@
 package dbservice
 
 import (
-	"encoding/json"
 	"time"
 )
 
+// make sure to update the date type in db when re-creating the table
 type RawDataEntity struct {
-	Symbol        string    `json:"symbol" db:"symbol, primarykey, size:6"`
-	Date          time.Time `json:"date" db:"dt, primarykey"`
-	Open          float64   `json:"open" db:"open"`
-	High          float64   `json:"high" db:"high"`
-	Low           float64   `json:"low" db:"low"`
-	Close         float64   `json:"close" db:"close"`
-	AdjustedClose float64   `json:"adjusted_close" db:"adjusted_close"`
-	Volume        int64     `json:"volume" db:"volume"`
-	Change        float64   `json:"change" db:"change"`
-	EMA           *float64  `json:"ema,omitempty" db:"-"`
-	EMA_20        *float64  `json:"ema20,omitempty" db:"ema20"`
-	EMA_50        *float64  `json:"ema50,omitempty" db:"ema50"`
-	EMA_100       *float64  `json:"ema100,omitempty" db:"ema100"`
-	EMA_200       *float64  `json:"ema200,omitempty" db:"ema200"`
-}
-
-func (e *RawDataEntity) ToString() string {
-	str, _ := json.Marshal(e)
-	return string(str)
+	Symbol          string    `json:"symbol" db:"symbol, primarykey, size:6"`
+	Date            time.Time `json:"date" db:"dt, primarykey"`
+	Open            float64   `json:"open" db:"open"`
+	High            float64   `json:"high" db:"high"`
+	Low             float64   `json:"low" db:"low"`
+	Close           float64   `json:"close" db:"close"`
+	AdjustedClose   float64   `json:"adjusted_close" db:"adjusted_close"`
+	Volume          int64     `json:"volume" db:"volume"`
+	Change          float64   `json:"change" db:"change"`
+	EMA             *float64  `json:"ema,omitempty" db:"-"`
+	EMA_20          *float64  `json:"ema20,omitempty" db:"ema20"`
+	EMA_50          *float64  `json:"ema50,omitempty" db:"ema50"`
+	EMA_100         *float64  `json:"ema100,omitempty" db:"ema100"`
+	EMA_200         *float64  `json:"ema200,omitempty" db:"ema200"`
+	CCI_100         *float64  `json:"cci100,omitempty" db:"cci100"`
+	AroonUp_50      *float64  `json:"aroonup_50,omitempty" db:"aroonup50"`
+	AroonDown_50    *float64  `json:"aroondown_50,omitempty" db:"aroondown50"`
+	Macd_20_200_100 *float64  `json:"macd20_200_100,omitempty" db:"macd20200100"`
 }
